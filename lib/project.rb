6ea2a47eb02c464b778b7f3e02631c34d6637ac8
AdamManselll <adammansellhmb@gmail.com>
2020-10-21 19:40:09 -0700

    Implement Project.find method

--- lib/project.rb
+++ lib/project.rb
@@ -15,12 +15,19 @@ class Project
   end
 
   class << self
+    def find(id)
+      project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+      if project
+        Project.new(title: project['title'], id: project['id'].to_i)
+      end
+    end
+
     def all
       projects = []
       result = DB.exec("SELECT * FROM projects;")
       if result
         result.each do |project|
-          projects << Project.new(title: project['title'], id: project['id'])
+          projects << Project.new(title: project['title'], id: project['id'].to_i)
         end
       end
       projects
